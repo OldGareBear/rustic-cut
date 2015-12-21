dc5efb06a42ebcc8f3d9eb2605189e99e90a8ea2
OldGareBear <betheagary@gmail.com>
2015-12-21 11:54:30 -0500

    master: start to fill out base html template; separate environment-specific from global settings
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- rustic_cut/rustic_cut/settings.py
+++ rustic_cut/rustic_cut/settings.py
@@ -10,49 +10,15 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+from rustic_cut.common import *
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+1nloazejpnfj6uvwx-@+my6x1in54asyw=1vza^8wzx9=d)yr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rustic_cut'
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'rustic_cut.urls'
-
-WSGI_APPLICATION = 'rustic_cut.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -64,21 +30,17 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+if not DEBUG:
+    # AWS_S3_SECURE_URLS = False
 
-TIME_ZONE = 'UTC'
+    DEFAULT_FILE_STORAGE = 'rustic-cut.storage.MediaRootS3Storage'
+    STATICFILES_STORAGE = 'rustic-cut.storage.StaticRootS3Storage'
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
+    STATIC_URL = '//rustic-cut-dev.s3.amazonaws.com/static/'
+    MEDIA_URL = '//rustic-cut-dev.s3.amazonaws.com/media/'
+else:
+    STATIC_URL = '//localhost:8000/static/'
+    MEDIA_URL = '//localhost:8000/media/'
+    STATIC_ROOT = '/static'
+    MEDIA_ROOT = '/media'
