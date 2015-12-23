b099d3accf17981a53bb9d72f16b12450ca591a7
OldGareBear <betheagary@gmail.com>
2015-12-22 20:27:01 -0500

    master: more stuff
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- rustic_cut/rustic_cut/common.py
+++ rustic_cut/rustic_cut/common.py
@@ -62,3 +62,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+)
