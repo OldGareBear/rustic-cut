f01e5c451e62839ce172cde6775845d95f5f6575
OldGareBear <betheagary@gmail.com>
2015-12-25 23:21:59 -0500

    master: add product page
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- rustic_cut/rustic_cut/admin.py
+++ rustic_cut/rustic_cut/admin.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name', 'categories')
+
     
 @admin.register(m.Category)
 class CategoryAdmin(admin.ModelAdmin):
