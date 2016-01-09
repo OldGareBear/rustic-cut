ca2f1e5b5ad550b15aaa8134e6e0ef6c3866f4c6
OldGareBear <betheagary@gmail.com>
2016-01-09 14:53:15 -0500

    master: add Databases to common.py
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- django/rustic_cut/rustic_cut/common.py
+++ django/rustic_cut/rustic_cut/common.py
@@ -78,3 +78,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 AWS_QUERYSTRING_AUTH = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
